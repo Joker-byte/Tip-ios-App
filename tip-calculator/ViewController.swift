144c91f4cb4de01d25dc16fdfeca6d4295231568
joker <gianlucadevios@gmail.com>
2026-01-18 11:43:23 +0100

    Initial Commit

---
+++ tip-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tip-calculator
+//
+//  Created by gdubioso dubioso on 14/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tip-calculatorTests/tip_calculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  tip_calculatorTests.swift
+//  tip-calculatorTests
+//
+//  Created by gdubioso dubioso on 14/01/26.
+//
+
+import Testing
+@testable import tip_calculator
+
+struct tip_calculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
